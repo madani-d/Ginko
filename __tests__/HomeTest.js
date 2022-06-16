e5abafcb4cfdef9939940bbdc7f483c61bbdb558
madani-d <madanidabouze@gmail.com>
2022-06-17 01:12:18 +0200

    core(circleci): add test

---
+++ __tests__/HomeTest.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Home from "../pages/index";
+
+describe("<Home/>", () => {
+  it("Check for Getting Started Text", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Get started by editing")).toBeInTheDocument();
+  });
+  it("Renders appropriately", () => {
+    render(<Home />);
+    expect(
+      screen.getByRole("heading", { name: "Welcome to Ginko !!!!" })
+    ).toBeInTheDocument();
+  });
+});
